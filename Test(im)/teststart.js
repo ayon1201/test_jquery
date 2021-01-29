2d8f144289eeccf92270368358e77c9a0148d286
ASHISHKUMAR M JHA <67501181+ayon1201@users.noreply.github.com>
2021-01-29 17:06:09 +0530

    Add files via upload

---
+++ Test(im)/teststart.js
@@ -0,0 +1,18 @@
+window.history.forward();
+function noBack() {
+    window.history.forward();
+}
+    // $('#terms_and_conditions').click(function(){
+    //     //If the checkbox is checked.
+    //     if($(this).is(':checked')){
+    //         //Enable the submit button.
+    //         $('#submit_button').attr("disabled", false);
+    //     } else{
+    //         //If it is not checked, disable the button.
+    //         $('#submit_button').attr("disabled", true);
+    //     }
+    // });
+    // function openLink() {
+    //     window.open('test.html', '1429893142534', 'width=' + (parseInt(window.innerWidth) * 0.3) + ',height=' + (parseInt(window.innerHeight) * .3) + ',toolbar=0,menubar=0,location=0,status=0,scrollbars=1,resizable=0,left=0,top=0');
+    //     return false;
+    // }
